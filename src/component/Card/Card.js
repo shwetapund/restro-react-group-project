f7aca717350a9c3ca2c9e72606681d6dc5c3ef94
prajktamhase <mhaseprajkta@gmail.com>
2023-09-13 16:14:30 +0800

    add reacr router dom

---
+++ src/component/Card/Card.js
@@ -0,0 +1,8 @@
+function Card(){
+    return(
+<div>
+    <h1>This is a card page</h1>
+</div>
+
+    )
+}
---
+++ src/view/Product/Product.js
@@ -0,0 +1,8 @@
+function Product(){
+    return(
+<div>
+    <h1>This is a project page...!</h1>
+</div>
+
+    )
+}
