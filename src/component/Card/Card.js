289c99bfaa2c526dd9b0e42960cc11928c065e6f
gayatrisathawane <gayatripsathawane@gmail.com>
2023-09-14 10:53:44 +0530

    check

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -9,6 +9,10 @@ export default function Card(imgc,text,price,distance,offer){
    <span className='text'>{text}</span> <span><button class='btn'>4.0 ☆</button></span><br/>
    <span className='price'>{price}</span>
    <span className='distance  '>🕑 {distance}  </span>
+   <h1>hhh</h1>
+
+   <h2>hhhhhhhh</h2>
+   <h2>jjjjj</h2>
   
    <hr/>
   <span className='offer'>{offer}</span>
