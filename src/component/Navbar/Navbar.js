44356da54486c8419fe0cc6d7d51983ad9a03a4c
Harshali Kanoje <harshalikanoje@gmail.com>
2023-09-14 17:50:56 -0700

    section div is added to home page

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg" >
       <div className="container">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">Taste Portal</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -3,8 +3,17 @@ import Navbar from '../../component/Navbar/Navbar';
 import './Home.css'
 // import Category from "../../../component/Category/Category";
 import Category from '../../component/Category/Category';
+import burger from './../../component/Category/Image/Smashburger-recipe.webp'
+import rolls from './../../component/Category/Image/Vegetable-Rolls.jpg'
+import cake from './../../component/Category/Image/cake.jpg'
+import thali from './../../component/Category/Image/Punjabi-thali.jpg'
+import chicken from './../../component/Category/Image/Pan-Fried-Chicken.webp'
+import sandwitch from './../../component/Category/Image/sandwitch.png'
 import ImgCat from './Image/pizza.jpg'
 import Service from '../../component/Service/Service';
+import fruit from './Image/Peach-pana.png'
+import truck from './Image/Logistics-rafiki.png'
+import pizzaman from './Image/Pizza maker-amico.png'
 
 export default function Home() {
     return (
@@ -15,36 +24,30 @@ export default function Home() {
             </div>
             <h1> Inspiration for your first order</h1>
             <div className='menuContainer'>
+                <Category DashImg={sandwitch} food={'Sandwitch'}/>
+                <Category DashImg={burger} food={'Burger'}/>
+                <Category DashImg={rolls} food={'Rolls'}/>
+                <Category DashImg={cake} food={'Cake'}/>
+                <Category DashImg={thali} food={'Thali'}/>
+                <Category DashImg={chicken} food={'Chicken'}/>
+                {/* <Category DashImg={ImgCat} />
                 <Category DashImg={ImgCat} />
-                <Category DashImg={ImgCat} />
-                <Category DashImg={ImgCat} />
-                <Category DashImg={ImgCat} />
-                <Category DashImg={ImgCat} />
-                <Category DashImg={ImgCat} />
+                <Category DashImg={ImgCat} /> */}
             </div>
 
-            <div className='service'>
-                <div className='container'>
-                    <h1>Our Service</h1>
-                    {/* <div className='row'>
-                        <div className='col-md-4'>
-                            <img src={ImgCat}/>
-                            <h3>QUALITY FOODS</h3>
-                            <p>Sit amet, consectetur adipiscing elit quisque eget maximus velit, non eleifend libero curabitur dapibus mauris sed leo cursus aliquetcras suscipit.</p>
-                        </div>
-                        <div className='col-md-4'>
-                        <h3>FASTEST DELIVERY</h3>
-                            <p>Sit amet, consectetur adipiscing elit quisque eget maximus velit, non eleifend libero curabitur dapibus mauris sed leo cursus aliquetcras suscipit.</p>
-                        </div>
-                        <div className='col-md-4'>
-                        <h3>ORIGINAL RECIPES</h3>
-                            <p>Sit amet, consectetur adipiscing elit quisque eget maximus velit, non eleifend libero curabitur dapibus mauris sed leo cursus aliquetcras suscipit.</p>
-                        </div>
-                    </div> */}
+
+
+            <div className='serviceCardContainer'>
+                <h1>Our Service</h1>
+                <div className='serviceContainer'>
+
+                    <Service img={fruit} heading={'QUALITY FOODS'} text={'Sit amet, consectetur adipiscing elit quisque eget maximus velit, non eleifend libero curabitur dapibus mauris sed leo cursus aliquetcras suscipit.'} />
+
+                    <Service img={truck} heading={'FASTEST DELIVERY'} text={'Sit amet, consectetur adipiscing elit quisque eget maximus velit, non eleifend libero curabitur dapibus mauris sed leo cursus aliquetcras suscipit.'} />
+
+                    <Service img={pizzaman} heading={'ORIGINAL RECIPES'} text={'Sit amet, consectetur adipiscing elit quisque eget maximus velit, non eleifend libero curabitur dapibus mauris sed leo cursus aliquetcras suscipit.'} />
                 </div>
             </div>
-
-            <Service img='<i class="fa-solid fa-truck"></i>' />
         </>
 
     )
