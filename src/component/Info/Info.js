621c4296991ff53c219a94d686c154709185be03
prajktamhase <mhaseprajkta@gmail.com>
2023-09-15 13:42:41 +0800

    fast food page finished

--- src/component/Info/Info.js
+++ src/component/Info/Info.js
@@ -1,15 +1,13 @@
 import "./Info.css";
 
 
-export default function Info({ img, name, text, price}) {
+export default function Info({ img}) {
     return (
         <>
             <div className="main-content">
                 <div>
                     <img className="images " src={img} />
-                    <h1><i>{name}</i></h1>
-                    <h3>{text}</h3>
-                    <h2>{price}</h2>
+                    
                 </div>
             </div>
         </>
--- src/view/Products/Nonveg/Nonveg.js
+++ src/view/Products/Nonveg/Nonveg.js
@@ -1,45 +1,75 @@
-import "./../Vegeterian/Vegeterian.css";
+import "./Nonveg.css";
 import Info from "../../../component/Info/Info";
 import Navbar from "./../../../component/Navbar/Navbar";
 import Button from "./../../../component/Button/Button";
+import Nonveg0 from "./Image/chiken5.jpg";
+import Nonveg1 from "./Image/chiken2.jpg";
+import Nonveg2 from "./Image/chiken3.webp";
+import Nonveg3 from "./Image/chiken7.png";
+import Nonveg4 from "./Image/chiken5.jpg";
+import Nonveg5 from "./Image/chiken6.jpg";
+import Biryani from "./Image/biryani5.jpg";
+import Biryani1 from "./Image/biryani2.jpg";
+import Biryani2 from "./Image/biryani6.jpg";
+import Biryani3 from "./Image/biryani5.jpg";
+import Biryani4 from "./Image/biryani2.jpg";
+import Biryani5 from "./Image/briyani0.jpg";
+import Egg from "./Image/egg1.jpg";
+import Egg1 from "./Image/egg2.jpg";
+import Egg2 from "./Image/egg3.jpg";
+import Egg3 from "./Image/egg4.jpg";
+import Egg4 from "./Image/egg5.jpg";
+import Egg5 from "./Image/egg6.jpg";
+import Egg6 from "./Image/egg1.jpg";
+import Mutton from "./Image/m1.png";
+import Mutton1 from "./Image/m2.png";
+import Mutton2 from "./Image/m3.png";
+import Mutton3 from "./Image/m4.png";
+import Mutton4 from "./Image/m5.png";
+import Mutton5 from "./Image/m6.png";
+import Mutton6 from "./Image/m7.png";
 
-export default function Nonveg() {
+
+
+export default function Nonveg(){
     return (
         <>
             <Navbar />
-            <h1 className="center green"><span className="brown">A</span>valiable-<span className="brown">T</span>o-<span className="brown">V</span>eg</h1><hr />
+            <h1 className="center green"><span className="brown">A</span>valiable-<span className="brown">T</span>o-<span className="brown">N</span>onveg</h1><hr />
             <div className="main-container">
                 <div>
-                    <img className="img"  />
+                    <img className="img"  src={Nonveg0}/>
                 </div>
                 <div>
-                    <h1 className="center green"><span className="brown">D</span>osa</h1>
-                    <p className="text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A dosa is a thin savory pancake in South Indian cuisine made from a fermented batter of ground black lentils and rice. Dosas are served hot, often with chutney and sambar. Dosa is a signature dish in South India and Sri Lanka, and is popularly served in their respective restaurants around the world.<br /><br />
-                        <h4>Place of origin: India</h4>
-                        <h4>Main ingredients: Urad dal, Rice</h4>
-                        <h4>Serving temperature:Hot</h4> </p>
+                    <h1 className="center green"><span className="brown">C</span>hiken</h1>
+                    <p className="text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The chicken is a domesticated species that arose from the red junglefowl, originally from India. They have also partially hybridized with other wild species of junglefowl. Rooster and cock are terms for adult male birds, and a younger male may be called a cockerel. A male that has been castrated is a capon.<br /><br />
+
+                        <h4>Scientific name: Gallus gallus domesticus</h4>
+                        <h4>Lifespan: 5 – 10 years</h4>
+                        <h4>Domain: Eukaryota</h4> 
+                        <h4>Order: Galliformes</h4></p>
                     <h4 className="front-text">Price:₹150</h4><br />
                     <Button text="Add To Cart" /><br />
                 </div>
 
             </div>
-            {/* <div className="main-container">
-                <Info img={Dosa} />
-                <Info img={Dosa} />
-                <Info img={Dosa1} />
-                <Info img={Dosa2} />
-                <Info img={Dosa3} />
-                <Info img={Dosa4} />
-            </div> */}
+            <div className="main-container">
+                <Info img={Nonveg1} />
+                <Info img={Nonveg2} />
+                <Info img={Nonveg3} />
+                <Info img={Nonveg4} />
+                <Info img={Nonveg5} />
+                <Info img={Nonveg2} />
+            </div>
             <hr /><hr /><hr />
 
 
             <div className="main-container">
                 <div>
-                    <img className="img"  />
+                    <img className="img" src={Biryani} />
                 </div>
                 <div>
-                    <h1 className="center green"><span className="brown">I</span>dli</h1>
+                    <h1 className="center green"><span className="brown">B</span>iryani</h1>
                     <p className="text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Idli or idly is a type of savoury rice cake, originating from South India, popular as a breakfast food in Southern India and in Sri Lanka. The cakes are made by steaming a batter consisting of fermented black lentils and rice.<br /><br />
                         <h4>Place of origin: India</h4>
                         <h4>Main ingredients: Rice, Urad dal</h4>
@@ -50,24 +80,24 @@ export default function Nonveg() {
                 </div>
 
             </div>
-            {/* <div className="main-container">
-                <Info img={Idli1} />
-                <Info img={Idli2} />
-                <Info img={Idli3} />
-                <Info img={Idli4} />
-                <Info img={Idli6} />
-                <Info img={Idli1} />
-            </div> */}
+            <div className="main-container">
+                <Info img={Biryani5} />
+                <Info img={Biryani1} />
+                <Info img={Biryani2} />
+                <Info img={Biryani3} />
+                <Info img={Biryani4} />
+                <Info img={Biryani5} />
+            </div>
             <hr /><hr /><hr />
 
 
 
             <div className="main-container">
                 <div>
-                    <img className="img"/>
+                    <img className="img" src={Egg}/>
                 </div>
                 <div>
-                    <h1 className="center green"><span className="brown">P</span>aratha</h1>
+                    <h1 className="center green"><span className="brown">E</span>gg</h1>
                     <p className="text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Idli or idly is a type of savoury rice cake, originating from South India, popular as a breakfast food in Southern India and in Sri Lanka. The cakes are made by steaming a batter consisting of fermented black lentils and rice.<br /><br />
                         <h4>Place of origin: India</h4>
                         <h4>Main ingredients: Rice, Urad dal</h4>
@@ -78,22 +108,22 @@ export default function Nonveg() {
                 </div>
 
             </div>
-            {/* <div className="main-container">
-                <Info img={Paratha1} />
-                <Info img={Paratha2} />
-                <Info img={Paratha3} />
-                <Info img={Paratha4} />
-                <Info img={Paratha5} />
-                <Info img={Paratha6} />
-            </div> */}
+            <div className="main-container">
+                <Info img={Egg1} />
+                <Info img={Egg2} />
+                <Info img={Egg3} />
+                <Info img={Egg4} />
+                <Info img={Egg5} />
+                <Info img={Egg6} />
+            </div>
             <hr /><hr /><hr />
 
             <div className="main-container">
                 <div>
-                    <img className="img"  />
+                    <img className="img" src={Mutton} />
                 </div>
                 <div>
-                    <h1 className="center green"><span className="brown">S</span>habudana <span className="brown">K</span>hichadi</h1>
+                    <h1 className="center green"><span className="brown">M</span>utton <span className="brown">C</span>urry</h1>
                     <p className="text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Idli or idly is a type of savoury rice cake, originating from South India, popular as a breakfast food in Southern India and in Sri Lanka. The cakes are made by steaming a batter consisting of fermented black lentils and rice.<br /><br />
                         <h4>Place of origin: India</h4>
                         <h4>Main ingredients: Rice, Urad dal</h4>
@@ -104,94 +134,15 @@ export default function Nonveg() {
                 </div>
 
             </div>
-            {/* <div className="main-container">
-                <Info img={ShabudanaKhichadi1} />
-                <Info img={ShabudanaKhichadi2} />
-                <Info img={ShabudanaKhichadi3} />
-                <Info img={ShabudanaKhichadi4} />
-                <Info img={ShabudanaKhichadi5} />
-                <Info img={ShabudanaKhichadi6} />
-            </div> */}
-            <hr /><hr /><hr />
-
-
             <div className="main-container">
-                <div>
-                    <img className="img" />
-                </div>
-                <div>
-                    <h1 className="center green"><span className="brown">R</span>ice</h1>
-                    <p className="text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Rice is the seed of the grass species Oryza sativa or, less commonly, O. glaberrima. The name wild rice is usually used for species of the genera Zizania and Porteresia, both wild and domesticated, although the term may also be used for primitive or uncultivated varieties of Oryza.<br /><br />
-                        <h4>Sources include: USDA</h4>
-                        <h4>Rice, white, long-grain, cooked</h4>
-                        <h4>130 Calories- 100 grams</h4>
-                        </p>
-
-                        <h4 className="front-text">Price:₹90</h4><br />
-                        <Button text="Add To Cart" /><br />
-                </div>
-
+                <Info img={Mutton1} />
+                <Info img={Mutton2} />
+                <Info img={Mutton3} />
+                <Info img={Mutton4} />
+                <Info img={Mutton5} />
+                <Info img={Mutton6} />
             </div>
-            {/* <div className="main-container">
-                <Info img={Rice1} />
-                <Info img={Rice2} />
-                <Info img={Rice3} />
-                <Info img={Rice4} />
-                <Info img={Rice5} />
-                <Info img={Rice3} />
-            </div> */}
             <hr /><hr /><hr />
-
-
-            <div className="main-container">
-                <div>
-                    <img className="img"/>
-                </div>
-                <div>
-                    <h1 className="center green"><span className="brown">P</span>oha</h1>
-                    <p className="text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Poha, also known as pauwa, sira, chira, chivda, aval or avalakki, among many other names, is flattened rice originating from the Indian subcontinent. Rice is parboiled before flattening so that it can be consumed with very little to no cooking.<br /><br />
-                        <h4>Origin: India</h4>
-                        <h4>Main ingredients: Dehusked rice</h4></p>
-                    <h4 className="front-text">Price:₹70</h4><br />
-                    <Button text="Add To Cart" /><br />
-                </div>
-
-            </div>
-            {/* <div className="main-container">
-                <Info img={Poha1} />
-                <Info img={Poha2} />
-                <Info img={Poha3} />
-                <Info img={Poha4} />
-                <Info img={Poha5} />
-                <Info img={Poha3} />
-            </div> */}
-            <hr /><hr /><hr />
-
-
-            <div className="main-container">
-                <div>
-                    <img className="img" />
-                </div>
-                <div>
-                    <h1 className="center green"><span className="brown">M</span>isal-Pav</h1>
-                    <p className="text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Misal pav is a dish from the Indian state of Maharashtra. It consists of misal and pav. The final dish is topped with farsan or sev, onions, lemon and coriander. It is usually served hot with bread or rolls toasted with butter and buttermilk or dahi and papad.<br /><br />
-                        <h4>Food energy (per serving): 289 cal kcal</h4>
-                        <h4>Associated cuisine: Indian</h4>
-                        <h4>Course: Breakfast, dinner</h4>
-                        <h4>Main ingredients: Sprouts, mutter, chick peas and chile powder gravy</h4> </p>
-                    <h4 className="front-text">Price:₹160</h4><br />
-                    <Button text="Add To Cart" /><br />
-                </div>
-
-            </div>
-            {/* <div className="main-container">
-                <Info img={Misal1} />
-                <Info img={Misal2} />
-                <Info img={Misal3} />
-                <Info img={Misal4} />
-                <Info img={Misal5} />
-                <Info img={Misal6} />
-            </div> */}
 
         </>
     )
